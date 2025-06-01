ac5a65c42005aacff15273a3d0c843c87178d32e
Ban Junwoo <bjw1055@gmail.com>
2025-06-01 22:34:18 +0900

    parseCSV

---
+++ exercise/team/team_test/back-end/parseCSV.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+module.exports = function parseCSV(filePath) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on('data', (row) => results.push(row))
+      .on('end', () => resolve(results))
+      .on('error', reject);
+  });
+};
