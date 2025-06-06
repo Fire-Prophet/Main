071671cc0ec7e2ea5f8a6896429c4bbf6e0b8862
Ban Junwoo <bjw1055@gmail.com>
2025-06-06 23:29:00 +0900

    buildLegend

---
+++ exercise/team/team_test/geo/buildLegend.js
@@ -0,0 +1,6 @@
+exports.generateLegend = () => [
+  { label: '낮음', color: '#4CAF50', range: [0, 1] },
+  { label: '보통', color: '#FFC107', range: [1, 2] },
+  { label: '높음', color: '#FF5722', range: [2, 3] },
+  { label: '위험', color: '#B71C1C', range: [3, 4] }
+];
