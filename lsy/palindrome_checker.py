2c87a21a89b18e4ab7c997454734ae45304d4580
Probius-ai <kcfm0529@naver.com>
2025-06-11 15:04:22 +0900

    delete conflict

--- lsy/palindrome_checker.py
+++
@@ -1,6 +0,0 @@
-def is_palindrome(text):
-    cleaned = ''.join(filter(str.isalnum, text.lower()))
-    return cleaned == cleaned[::-1]
-
-s = input("문장을 입력하세요: ")
-print("회문입니다." if is_palindrome(s) else "회문이 아닙니다.")
