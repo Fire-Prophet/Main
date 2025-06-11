8bcd38d3a89f35ac436246bed77f03c67f83e23e
Probius-ai <kcfm0529@naver.com>
2025-06-11 20:14:09 +0900

    a

--- Project/backend/apiServer.js
+++ Project/backend/apiServer.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mysql = require('mysql2/promise');
 const turf = require('@turf/turf');
 const db = require('./firebaseAdmin');
-const { mountainStationsData } = require('./mountainStations');
+const { mountainStationsData } = require('./mountainStationsData');
 
 class PriorityQueue {
     constructor() { this.elements = []; }
